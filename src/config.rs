adf045b1101ce0eafbdb4c58e3d6468a66b38b5e
xiaoxiaoxin <2083864628@qq.com>
2025-07-03 08:23:54 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["222.173.140.166"];
+pub const RS_PUB_KEY: &str = "F1+ywyjIYJWgr7SbuFh5pbFuE7fls3EMwpiX0pPkK74=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
